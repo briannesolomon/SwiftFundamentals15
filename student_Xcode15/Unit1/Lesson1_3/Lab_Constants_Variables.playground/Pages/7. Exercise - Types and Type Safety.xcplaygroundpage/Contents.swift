a18f82152910ad3d193a05b00e08e3f7ba28d7b3
briannesolomon <solomonb2948@durhamtech.edu>
2024-10-23 18:08:40 -0400

    half of 1-3 and 1-4

--- student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -7,17 +7,14 @@ var firstDecimal = 1.1
 var secondDecimal = 2.2
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-var trueOrFalse = true 
-firstDecimal = trueOrFalse
-
+var trueOrFalse = true
+print("No it does not compile because you cannot assign one type to a different type.")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var name = "Bri"
-firstDecimal = name
-
+print("No it does not compile because you cannot assign one type to a different type.")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var number = 5
-firstDecimal = number
-
+print("No it does not compile because you cannot assign one type to a different type.")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
  */
--- student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -10,10 +10,10 @@ print(name)
 print(name)
 
 //:  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
-var distanceTraveled = 0
+var distanceTraveled: Double = 54.3
 
 //:  Now assign a value of 54.3 to `distanceTraveled`. Does the code compile? Go back and set an explicit type on `distanceTraveled` so the code will compile.
-var distanceTraveled: Double = 54.3 
+
 
 /*:
 [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
