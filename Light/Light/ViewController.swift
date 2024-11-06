e53d061ac787ab2a81411057f9241982a95c1244
briannesolomon <solomonb2948@durhamtech.edu>
2024-11-06 17:13:50 -0500

    Light Project

---
+++ Light/Light/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by remotestudent on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        // Do any additional setup after loading the view.
+    }
+
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
+}
+
