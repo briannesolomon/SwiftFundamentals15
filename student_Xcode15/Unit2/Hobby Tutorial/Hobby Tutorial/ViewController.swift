f70f146e240fe5f9be11e0c0f8524947d2eedc78
briannesolomon <solomonb2948@durhamtech.edu>
2024-12-08 11:24:36 -0500

    Hobby Tutorial

---
+++ student_Xcode15/Unit2/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by RemoteStudent on 12/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
